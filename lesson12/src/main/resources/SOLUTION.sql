35f6cbfa0c4aa2fae130764caf52ba9542c32075
kukumaj <zakumaj@gmail.com>
2022-12-07 22:55:22 +0100

    done!

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,5 @@
-DELETE FROM student WHERE id IN (SELECT DISTINCT m.student_id FROM mark m  WHERE grade > 4);
-DELETE FROM student WHERE id IN (SELECT DISTINCT m.student_id FROM mark m  WHERE mark < 4);
+UPDATE mark set studemt.id = null where id in (SELECT DISTINCT m.id FROM mark m JOIN subject s on s.id = m.subject_id WHERE grade >= 4);
+DELETE FROM student WHERE id not IN (SELECT DISTINCT m.student_id FROM mark);
+DELETE FROM student WHERE id IN (SELECT  DISTINCT s.id FROM student s JOIN mark m ON s.id = m.student_id WHERE m.mark < 4);
 DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;
